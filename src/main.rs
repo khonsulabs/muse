e04f032da52f9a1fe0e8d98613e28a7dc0ac1af8
Jonathan Johnson <jon@khonsulabs.com>
2024-02-22 16:02:04 -0800

    Added basic repl
    It doesn't highlight error ranges or anything.
    
    Refs #10

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,80 @@
 use muse::compiler::Compiler;
+use muse::syntax::{parse, Ranged};
 use muse::vm::Vm;
+use rustyline::completion::Completer;
+use rustyline::error::ReadlineError;
+use rustyline::highlight::Highlighter;
+use rustyline::hint::Hinter;
+use rustyline::history::DefaultHistory;
+use rustyline::validate::{ValidationContext, ValidationResult, Validator};
+use rustyline::{Editor, Helper};
 
 fn main() {
+    let mut editor: Editor<Muse, DefaultHistory> = Editor::new().unwrap();
+    editor.set_helper(Some(Muse));
     let mut vm = Vm::default();
-    let code = Compiler::compile(
-        r"
-        {
-            var a = 0 + 1;
-            a = 2;
-            a + b
+    let mut compiler = Compiler::default();
+    loop {
+        match editor.readline("> ") {
+            Ok(line) => {
+                compiler.push(&line);
+                match compiler.build() {
+                    Ok(code) => match vm.execute(&code) {
+                        Ok(value) => {
+                            let displayed = value
+                                .map_str(&mut vm, |_vm, value| {
+                                    println!("{value}");
+                                })
+                                .is_ok();
+                            if !displayed {
+                                println!("{value:?}");
+                            }
+                        }
+                        Err(err) => {
+                            eprintln!("Execution error: {err:?}");
+                        }
+                    },
+                    Err(err) => {
+                        eprintln!("Compilation error: {err:?}");
+                    }
+                }
+            }
+            Err(ReadlineError::Eof) => break,
+            Err(ReadlineError::Interrupted) => {}
+            Err(other) => unreachable!("unexpected error: {other}"),
         }
-        ",
-    )
-    .unwrap();
-    let result = vm.execute(&code).unwrap();
-    assert_eq!(result.as_i64(), Some(5));
+    }
+}
+
+struct Muse;
+
+impl Helper for Muse {}
+
+impl Validator for Muse {
+    fn validate(&self, ctx: &mut ValidationContext) -> rustyline::Result<ValidationResult> {
+        match parse(ctx.input()) {
+            Ok(_) => Ok(ValidationResult::Valid(None)),
+            Err(Ranged(muse::syntax::Error::UnexpectedEof, _)) => Ok(ValidationResult::Incomplete),
+            Err(err) => Ok(ValidationResult::Invalid(Some(format!(
+                "@{}:{}: {:?}",
+                err.1.start,
+                err.1.end(),
+                err.0
+            )))),
+        }
+    }
+
+    fn validate_while_typing(&self) -> bool {
+        true
+    }
+}
+
+impl Highlighter for Muse {}
+
+impl Hinter for Muse {
+    type Hint = String;
+}
+
+impl Completer for Muse {
+    type Candidate = String;
 }
